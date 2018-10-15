83c97aea280164888e3a9e31f074d7c1b3b75daa
ida <poohmaki0506@gmail.com>
2018-10-15 20:06:26 +0900

    データロードまで

---
+++ questtionnaire_analysis/data_load.py
@@ -0,0 +1,29 @@
+import sys
+import csv
+from pathlib import Path
+
+
+def _loads(fn):
+    datas = {}
+
+    if not fn.exists():
+        print("[FILE ERROR]", fn, "is not found.")
+        sys.exit()
+    with fn.open("r") as f:
+        reader = csv.reader(f)
+        header = next(reader)
+        for row in reader:
+            datas.append(row)
+    return header, datas
+
+
+def data_load():
+    fn = Path("/Users/ida/Dropbox")
+    questions = []
+
+    for week in ["201810_1week", "201810_2week"]:
+        for nth in ["questionnair_middle.csv", "questionnair_last.csv"]:
+            header, data = _loads(fn / week / nth)
+            questions.append(data)
+
+    return header, questions
---
+++ questtionnaire_analysis/main.py
@@ -0,0 +1,9 @@
+import sys
+import data_load
+
+
+def main():
+    data_load.data_load()
+
+if __name__ == "__main__":
+    main()
