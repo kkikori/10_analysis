c3ee8d90df0d7309e4eccad1c95ccb916d96b375
ida <poohmaki0506@gmail.com>
2018-10-16 13:42:28 +0900

    データロードまで

--- questtionnaire_analysis/main.py
+++ questtionnaire_analysis/main.py
@@ -1,9 +1,15 @@
 import sys
 import data_load
+import quote
 
 
 def main():
-    data_load.data_load()
+    header, datas = data_load.data_load()
+    week1 = {"middle": datas[0], "last": datas[1]}
+    week2 = {"middle": datas[2], "last": datas[3]}
+
+    quote(datas=week1, header=header)
+
 
 if __name__ == "__main__":
     main()
---
+++ questtionnaire_analysis/quote.py
@@ -0,0 +1,4 @@
+def quote(header, datas):
+    print(header[-3])
+
+    d = datas["middle"]
