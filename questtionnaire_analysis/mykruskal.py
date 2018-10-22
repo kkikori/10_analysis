a7d3c204c658ece3dd0b6a2433ebb1019858e51a
ida <poohmaki0506@gmail.com>
2018-10-22 13:56:07 +0900

    取り出すとこまで完成

--- questtionnaire_analysis/mykruskal.py
+++ questtionnaire_analysis/mykruskal.py
@@ -1,18 +1,16 @@
 import numpy as np
 
-LIKERT_EVAL_IDX = {
-    "middle": {
-        "total": [4, 5, 6, 7, 8, 9],
-        "agent": [12, 13, 14, 15, 16, 17],
-        "system": [19, 20],
-        "estimator": [21, 22, 23, 24, 25, 26, 27, 28, 29]
-    },
-    "last": {
-        "total": [2, 3, 4, 5, 6, 7],
-        "agent": [10, 11, 12, 13, 14, 15],
-        "estimator": [22, 23, 24, 25, 26, 27, 28, 29, 30],
-        "system": [17, 18, 19]
-    }
+LIKERT_EVAL_IDX_MIDDLE = {
+    "total": [4, 5, 6, 7, 8],
+    "agent": [12, 13, 14, 15, 16, 17],
+    "system": [19, 20],
+    "estimator": [21, 22, 23, 24, 25, 26, 27, 28, 29]
+}
+LIKERT_EVAL_IDX_LAST = {
+    "total": [2, 3, 4, 5, 6],
+    "agent": [10, 11, 12, 13, 14, 15],
+    "estimator": [22, 23, 24, 25, 26, 27, 28, 29, 30],
+    "system": [17, 18, 19]
 }
 
 EVAL_FREE_IDX = {
@@ -31,12 +29,38 @@ EVAL_FREE_IDX = {
 }
 
 
+# データの取り出し
 def _extract_total_eval(week1, week2):
-    week1_evals =
+    we1 = np.array(week1["middle"])
+    week1_claim = we1[:, LIKERT_EVAL_IDX_MIDDLE["total"][0]:LIKERT_EVAL_IDX_MIDDLE["total"][-1] + 1].tolist()
+    we1 = np.array(week1["last"])
+    week1_random = we1[:, LIKERT_EVAL_IDX_LAST["total"][0]:LIKERT_EVAL_IDX_LAST["total"][-1] + 1].tolist()
+
+    we2 = np.array(week2["middle"])
+    week2_random = we2[:, LIKERT_EVAL_IDX_MIDDLE["total"][0]:LIKERT_EVAL_IDX_MIDDLE["total"][-1] + 1].tolist()
+    we2 = np.array(week2["last"])
+    week2_claim = we2[:, LIKERT_EVAL_IDX_LAST["total"][0]:LIKERT_EVAL_IDX_LAST["total"][-1] + 1].tolist()
+
+    # 数値に変換
+    week1_claim.extend(week2_claim)
+    claim_list =[]
+    for person in week1_claim:
+        claim_list.append([int(e) for e in person])
+    week1_random.extend(week2_random)
+    random_list=[]
+    for person in week1_random:
+        random_list.append([int(e) for e in person])
+
+    print(claim_list)
+
+    return claim_list, random_list
 
 
 def eval_total(week1, week2):
     claim_evals, random_evals = _extract_total_eval(week1, week2)
+
+    print("claim_evals")
+    [print(c) for c in claim_evals]
 
 
 def significant_difference(week1, week2, middle_header, last_header):
@@ -46,7 +70,9 @@ def significant_difference(week1, week2, middle_header, last_header):
     [print(hi, hea) for hi, hea in enumerate(last_header)]
 
     claim_agent = {"week1": week1["middle"]}
-    #claim_agent.extend(week2["last"])
+    # claim_agent.extend(week2["last"])
 
-    #random_agent = week1["last"]
-    #random_agent.extend(week2["middle"])
+    # random_agent = week1["last"]
+    # random_agent.extend(week2["middle"])
+
+    eval_total(week1, week2)
