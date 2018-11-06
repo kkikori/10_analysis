03e6f7c2e72531da1c8abb33b58125f1bddcc30e
ida <poohmaki0506@gmail.com>
2018-11-06 17:49:51 +0900

    ファイルパスなどの準備

--- data_analysis_main.py
+++ data_analysis_main.py
@@ -1,11 +1,29 @@
 import sys
 import data_analysis
+from pathlib import Path
+import simplejson as json
 
 
+def _preparate_path():
+    fn = Path("file_paths.json")
+    f = fn.open("r")
+    jsonData = json.load(f)
+
+    week_paths = []
+    for week in jsonData:
+        data_root = week.pop("root")
+        root_path = Path(data_root)
+        for rorc, v in week.items():
+            paths = {}
+            for fn, fp in v.items():
+                paths[fn] = root_path / fp
+        week_paths[rorc] = paths
 
 
 def main():
-    data_analysis.data_load()
+    paths_l = _preparate_path()
+    data_analysis.data_load(paths_l)
+
 
 if __name__ == "__main__":
     main()
